16e32985f95be8ca00ed4b350e648cd81477efa3
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-06 16:59:25 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,15 +1,18 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def initialize
-    self.class.all << self
+    def count
+      self.all.count
+    end
   end
 
-  def reset_all
-    self.all.clear
-  end
-
-  def count
-    self.all.count
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
 
 end
